787129c796b29b3cb393ca4740416fc21c24a59d
eun-n <eunicena.12@gmail.com>
2016-05-10 16:53:33 -0700

    korean text not compatible

--- Twice/config/routes.rb
+++ Twice/config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
 
   root 'lyrics#index'
 
+    resources :lyrics
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
